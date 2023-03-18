4bb308f30ec5a3fe14c670ebd33c189070c5c4da
alex11g <duranmateo50@gmail.com>
2023-03-18 11:48:55 -0500

    pagina-tienda-index-detalles

---
+++ js/index.js
@@ -0,0 +1,40 @@
+const {createApp}= Vue
+
+
+const app= createApp({
+
+    data(){
+
+        return{
+            datos :[],
+            categorias:[],
+            checkValue:[],
+            resultado:[undefined],
+            filtrotexto:"",
+            disponible : []
+
+        }
+    },
+    created(){
+        fetch("https://mindhub-xj03.onrender.com/api/petshop")
+            .then(response => response.json())
+            .then((data) =>{
+                this.datos = data
+                this.categorias = [...new Set(this.datos.map(key => key.categoria))]
+                this.disponible = this.datos.filter(key => key.disponibles < 5)
+                console.log(this.datos)
+                this.resultado = data
+                
+            })
+    },
+    methods:{
+        
+        filtrobusqueda(){
+            this.resultado = this.datos.filter(key => {
+                return key.producto.toLowerCase().includes(this.filtrotexto.toLowerCase())
+                && (this.checkValue.includes(key.categoria) || this.checkValue.length == 0)
+            })
+        }
+    }
+})
+app.mount("#andres")
