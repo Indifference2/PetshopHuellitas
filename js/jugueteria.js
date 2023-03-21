1e46ac9a559c78084604006ebdca8fccb292106f
Iván <102835891+Indifference2@users.noreply.github.com>
2023-03-21 15:43:27 -0300

    Agregado carrito a la jugueteria

--- js/jugueteria.js
+++ js/jugueteria.js
@@ -32,6 +32,41 @@ const app= createApp({
                 return key.producto.toLowerCase().includes(this.filtrotexto.toLowerCase())
              
             })
+        }agregarAlCarrito(item){
+            if(!this.productoEstaRepetidoPorId(item._id)){
+                this.carrito.push({
+                    nombre : item.producto,
+                    cantidadDisponible : item.disponibles,
+                    contadorBoton : 1,
+                    id : item._id,
+                    imagen : item.imagen,
+                    precio : item.precio,
+                })
+            }else{
+                item.contadorBoton + 1
+            }
+            console.log(this.carrito)
+        },
+        productoEstaRepetidoPorId(productoId){
+            return this.carrito.some(item => item.id === productoId)
+        },
+
+        agregarCantidadProducto(producto){
+            if(producto.cantidadDisponible > producto.contadorBoton){
+                producto.contadorBoton += 1
+            }
+        },
+        eliminarDelCarrito(producto){
+            if(producto.contadorBoton > 1 ){
+                producto.contadorBoton -= 1
+            }else{
+                this.carrito =  this.carrito.filter(item => !(item.id === producto.id))
+            }
+        },
+    },
+    computed:{
+        handleCarrito(){
+            localStorage.setItem("carrito", JSON.stringify(this.carrito))
         }
     }
 })
