9d82e79a55ed80fd4355d9a32c0caa45ed0b5823
Iván <102835891+Indifference2@users.noreply.github.com>
2023-03-21 18:23:34 -0300

    Cambiada la ruta del logo en todas las paginas

--- js/farmacia.js
+++ js/farmacia.js
@@ -21,17 +21,53 @@ const app= createApp({
                 this.datos = data
                 this.farmacia = this.datos.filter(key => key.categoria== "farmacia")
                 this.resultado =this.farmacia
-                
             })
+            this.carrito=JSON.parse(localStorage.getItem("carrito")) || []
     },
     methods:{
         
         filtrobusqueda(){
             this.resultado = this.farmacia.filter(key => {
                 return key.producto.toLowerCase().includes(this.filtrotexto.toLowerCase())
-             
             })
-        }
+            
+        },
+        agregarAlCarrito(item){
+            if(!this.productoEstaRepetidoPorId(item._id)){
+                this.carrito.push({
+                    nombre : item.producto,
+                    cantidadDisponible : item.disponibles,
+                    contadorBoton : 1,
+                    id : item._id,
+                    imagen : item.imagen,
+                    precio : item.precio,
+                })
+            }else{
+                item.contadorBoton + 1
+            }
+            console.log(this.carrito)
+        },
+        productoEstaRepetidoPorId(productoId){
+            return this.carrito.some(item => item.id === productoId)
+        },
+
+        agregarCantidadProducto(producto){
+            if(producto.cantidadDisponible > producto.contadorBoton){
+                producto.contadorBoton += 1
+            }
+        },
+        eliminarDelCarrito(producto){
+            if(producto.contadorBoton > 1 ){
+                producto.contadorBoton -= 1
+            }else{
+                this.carrito =  this.carrito.filter(item => !(item.id === producto.id))
+            }
+        },
+    },
+    computed:{
+        handleCarrito(){
+            localStorage.setItem("carrito", JSON.stringify(this.carrito))
+    }
     }
 })
 app.mount("#andres")
