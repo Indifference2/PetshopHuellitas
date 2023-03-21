6ce76b2f14583887dfcacb6dc5324805b8ae174a
alex11g <duranmateo50@gmail.com>
2023-03-21 12:23:41 -0500

    farmacia-jugueteria

---
+++ js/farmacia.js
@@ -0,0 +1,37 @@
+const {createApp}= Vue
+
+
+const app= createApp({
+
+    data(){
+
+        return{
+            datos :[],
+            farmacia : [],
+            resultado:[undefined],
+            filtrotexto:""
+           
+
+        }
+    },
+    created(){
+        fetch("https://mindhub-xj03.onrender.com/api/petshop")
+            .then(response => response.json())
+            .then((data) =>{
+                this.datos = data
+                this.farmacia = this.datos.filter(key => key.categoria== "farmacia")
+                this.resultado =this.farmacia
+                
+            })
+    },
+    methods:{
+        
+        filtrobusqueda(){
+            this.resultado = this.farmacia.filter(key => {
+                return key.producto.toLowerCase().includes(this.filtrotexto.toLowerCase())
+             
+            })
+        }
+    }
+})
+app.mount("#andres")
